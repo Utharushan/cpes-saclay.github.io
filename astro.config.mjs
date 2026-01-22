3dbd6471b37be5fc3f8a437a1aaad9df87497a0f
Funasitien DreamCloud <hello@funa.dev>
2026-01-22 13:53:55 +0100

    🚧 Push

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,18 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
 import mdx from "@astrojs/mdx";
+import remarkCallout from "@r4ai/remark-callout";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
+  },
+  markdown: {
+    remarkPlugins: [remarkCallout],
+    shikiConfig: {
+      theme: "css-variables"
+    }
   },
   integrations: [icon(), mdx()],
   //Uniquement pour gitlab
